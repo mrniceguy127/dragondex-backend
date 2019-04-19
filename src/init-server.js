7bd60e30c3edbbb9c40b68f4b985c5b7a049f1e9
Matthew Kleiner <mkleiner2670@gmail.com>
2019-04-19 15:45:30 -0400

    Have basic Route class and added Server class. One route is currently being used for demo purposes (/api/v1/art).

--- src/init-server.js
+++ src/init-server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
+const dragondexLib = require('../lib');
+
+
+let server = new dragondexLib.Server(process.env.PORT || 3000, '/', './routes');
+server.registerRoutes();
+server.listen('Server running on port ' + server.port + '!');
