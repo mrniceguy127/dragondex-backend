c1e704d794c62301e59d4fd4680b11dd89b392b5
Matthew Kleiner <mkleiner2670@gmail.com>
2019-04-30 15:16:58 -0400

    Added test api user endpoint (/api/v1/user/:userId) to get user info based on an ID.

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
 const dragondexLib = require('../../../lib');
-const APIRoute = dragondexLib.route.APIRoute;
-const UserModel = dragondexLib.db.models.User;
+const APIRoute = dragondexLib.routes.APIRoute;
+// const UserModel = dragondexLib.db.models.User;
+
+
+// The /user sub route for the base API route.
+
+module.exports = class ArtAPIRoute extends APIRoute {
+  constructor(app) {
+    super(app);
+    this.path = 'user/:userId';
+    this.type = 'GET';
+  }
+
+  async action(req, res, next) {
+    let userId = req.params.userId;
+    let userData = {
+      id: userId,
+      artwork: [
+        'testId1',
+        'testId2',
+        'testId3',
+        'testId4'
+      ],
+      name: "Jim Son",
+      username: "js203"
+    }
+
+    res.json(userData);
+  }
+}
