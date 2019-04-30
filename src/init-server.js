50163a9717b1d29125278e0b571ba894041dadcd
Matthew Kleiner <mkleiner2670@gmail.com>
2019-04-30 15:06:17 -0400

    Added rate limit to API endpoints of 60 requests max per 1 minute. Added start script to package.json.

--- src/init-server.js
+++ src/init-server.js
@@ -3,10 +3,20 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const dragondexLib = require('../lib');
+const rateLimit = require('express-rate-limit');
 
 mongoose.connect(process.env.MONGODB_SERVER || 'mongodb://localhost/dragondex', { useNewUrlParser: true });
 
 let server = new dragondexLib.Server(process.env.PORT || 3000, '/', __dirname + '/routes');
+
+// Add ratelimiting for API endpoints. 60 requests max / minute.
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 60
+});
+
+server.app.use('/api/v1/', apiLimiter);
+
 server.registerRoutes();
 server.app.use('/static', express.static(path.join(__dirname, './public')));
 server.listen('Server running on port ' + server.port + '!');
